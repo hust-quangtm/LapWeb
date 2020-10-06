5bfd48b62f795f06a506d774d23f6f5d57b38892
hapo-quangtm <68210682+hapo-quangtm@users.noreply.github.com>
2020-10-06 07:53:50 +0700

    Lap3

---
+++ LapProject/Lap3/basicform.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Simple Basic Form</title>
+</head>
+<body>
+    <form action="https://www.facebook.com/quang.quangtm" method="POST">Click submit to start our initial PHP program
+    <br>
+    <input type="text" name="NAME">
+    <br>
+    <input type="submit" value="click to submit">
+    <input type="reset" value=""Erase and restart>
+    </form>
+</body>
+</html>
---
+++ LapProject/Lap3/stringtest.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StringTest</title>
+</head>
+<body>
+    <?PHP
+        echo "Hello\n";
+        echo "This is </br> string test";
+    ?>
+</body>
+</html>
